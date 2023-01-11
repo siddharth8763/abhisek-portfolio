3df188dab9733d9328ae35fc6a870c29a0f543dc
Siddharth <siddharth8763@gmail.com>
2023-01-11 14:35:33 +0530

    Placeholder Created for Necessary Changes

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useContext,useState,useEffect } from 'react'
+import { ThemeContext } from './contexts/theme'
+import Header from './components/Header/Header'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+import Skills from './components/Skills/Skills'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import Preload from './components/Preload/Pre'
+import './App.css'
+
+const App = () => {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const [{ themeName }] = useContext(ThemeContext)
+
+  return (
+    <>
+      <Preload load={load} />
+      <div id='top' className={`${themeName} app`}>
+      <Header />
+
+      <main>
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
+
+      <ScrollToTop />
+      <Footer />
+    </div>
+    </>
+  )
+}
+
+export default App
